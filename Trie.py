a59e51300f22b71da4416d912426144b645c66a7
Vlad Pugach <v.pugach@ukma.edu.ua>
2019-05-23 13:08:10 +0300

    Remove unnecessary and fix find_prefix_node

--- Trie.py
+++ Trie.py
@@ -27,10 +27,14 @@ def find_prefix_node(root: Trie, prefix: str):
     if not root.children:
         return None
     for char in prefix:
+        found = False
         for child in node.children:
             if child.char == char:
+                found = True
                 node = child
                 break
+        if not found:
+            return None
     return node
 
 def traverse_trie(root: Trie, num: int, prefix: str):
@@ -46,29 +50,16 @@ def traverse_trie(root: Trie, num: int, prefix: str):
 def find_first_n_after_prefix(root: Trie, n: int, prefix: str):
     node = find_prefix_node(root, prefix)
     if node is not None:
-        print(node.char)
         yield from traverse_trie(node, n, prefix)
-
-
-def traverse(root: Trie):
-    print(root.char, root.isLeaf, root.children.__len__(), "\n")
-    for node in root.children:
-        traverse(node)
-
-def k(n: int):
-    if n == 0:
-        return
-    n -= 1
-    yield n
-    yield from k(n)
-
 
 
 if __name__ == "__main__":
 
     root = Trie('')
     add(root, "kek")
-    print(find_prefix_node(root, "").char)
+    add(root, "kek212")
+    add(root, "kekasd")
+    add(root, "kek3")
 
-    for s in find_first_n_after_prefix(root, 10, ""):
+    for s in find_first_n_after_prefix(root, 10, "keka"):
         print(s)
