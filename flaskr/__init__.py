effbc53c63baf23e9cf4e327dc9e9d4ebc92a814
Vlad Pugach <v.pugach@ukma.edu.ua>
2019-05-23 22:43:53 +0300

    Flask + login

---
+++ flaskr/__init__.py
@@ -0,0 +1,35 @@
+import os
+from . import db
+
+from flask import Flask
+from flask_pymongo import PyMongo
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev_key',
+        MONGO_URI=os.environ.get('DATABASE_URL') or "mongodb://localhost:27017/first_task"
+    )
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import home
+    app.register_blueprint(home.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    db.mongo = PyMongo(app)
+    return app
